2cb25e0ec1e772e461d042417ff02db74d8aff04
Hugo Zonderland <info@hugozonderland.nl>
2013-05-09 23:24:08 +0200

    Created IMDb class to fetch data from IMDbAPI.org and IMDbShow as class for its data.

---
+++ IMDbApiLib/IMDb.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * Created by JetBrains PhpStorm.
+ * User: hugozonderland
+ * Date: 08-05-13
+ * Time: 12:18
+ * To change this template use File | Settings | File Templates.
+ */
+
+class IMDbApi {
+
+    /**
+     * Where to send all the requests to.
+     */
+    const IMDBAPI = 'http://imdbapi.org/';
+
+    /**
+     * Contains all the data fetched in the request
+     * @var SimpleXML file
+     */
+    private $_IMDBXML;
+
+    /**
+     * Fetches data from the IMDBAPI.org website, creates XML file and stores the information
+     * @param $imDbId
+     */
+    public function getSerieById($imDbId) {
+        $url = self::IMDBAPI . '?id=' . $imDbId . '&type=xml&plot=full&episode=1&lang=en-US&aka=simple&release=simple&business=1&tech=1';
+        $this->_IMDBXML = SimpleXML::get_xml_url_contents($url);
+    }
+}
