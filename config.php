d2195987564e535a83b1d66b3cd95ad1f2832697
Hugo Zonderland <info@hugozonderland.nl>
2013-07-05 12:06:48 +0200

    New config version.

--- config.php
+++ config.php
@@ -2,16 +2,22 @@
 /**
  * The TV Db API key to use. You can register for one here: http://thetvdb.com/?tab=apiregister
  */
-define('TVDB_API_KEY',          '');
-define('BIERDOPJE_API_KEY',     '');
+define('TVDB_API_KEY', '');
+define('BIERDOPJE_API_KEY', '');
 
-define('USER_AGENT',            'TweakerSerieTopicGenerator/1.0');
-define('ICON_URL',              'http://icon.ultimation.nl/');
+define('USER_AGENT', 'TweakerSerieTopicGenerator/1.0');
+define('ICON_URL', 'http://icon.ultimation.nl/');
+
+/**
+ * TRAKT API
+ */
+define('TRAKT_API_KEY', '');
 
 // Date time some for comparison
 date_default_timezone_set('Europe/Amsterdam');
 
 // Tools
+require_once dirname(__FILE__) . '/tools/DebugOutput.tool.php';
 require_once dirname(__FILE__) . '/tools/C_URL.tool.php';
 require_once dirname(__FILE__) . '/tools/SimpleXML.tool.php';
 require_once dirname(__FILE__) . '/tools/ArrayToString.tool.php';
@@ -29,9 +35,12 @@ require_once dirname(__FILE__) . '/lib/IMDbApiLib/IMDb.php';
 require_once dirname(__FILE__) . '/lib/IMDbApiLib/IMDbShow.php';
 require_once dirname(__FILE__) . '/lib/IMDbApiLib/IMDbEpisode.php';
 
+// Trakt
+require_once dirname(__FILE__) . '/lib/TraktApiLib/trakt.php';
+require_once dirname(__FILE__) . '/lib/TraktApiLib/traktShow.php';
+
 // Tweakers
 require_once dirname(__FILE__) . '/lib/TweakersLib/TweakersUBB.php';
 require_once dirname(__FILE__) . '/lib/TweakersLib/TweakersSerie.php';
-
 
 ?>
